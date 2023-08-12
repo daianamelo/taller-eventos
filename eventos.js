695b2198e8ae6b958b9b7a071738fc5429f4151b
daianamelodev <59324653+daianamelo@users.noreply.github.com>
2023-08-12 18:23:42 +0200

    funco

--- eventos.js
+++ eventos.js
@@ -1,3 +1,13 @@
 function saludar() {
     alert('¡Hola!');
 }
+
+contenedor=document.getElementById('contenedor')
+boton=document.getElementById('boton')
+
+
+boton.addEventListener('click',function(){
+
+    alert( "Hola! Soy el div")
+ 
+ })
